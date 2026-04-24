76734ecfc85362f4f4f5f2c78f39e1ba4bab8463
Marwa Al Jaouni <marwa.aljaouni@outlook.com>
2026-04-24 16:26:51 +0200

    fix javaMailSender

--- backend/src/test/java/com/backend/eventmarketplace/EventmarketplaceApplicationTests.java
+++ backend/src/test/java/com/backend/eventmarketplace/EventmarketplaceApplicationTests.java
@@ -4,12 +4,17 @@ import com.backend.eventmarketplace.config.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestConfig.class)
 class EventmarketplaceApplicationTests {
+
+    @MockitoBean
+    JavaMailSender mailSender;
 
     @Test
     void contextLoads() {
